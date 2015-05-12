9b28b6319dc92c9c750247ba94a5036bf87c94c9
EthanHess <ech1988@gmail.com>
2015-05-12 15:56:31 -0600

    core data model set up

---
+++ DayXTrioSwift/DayXTrioSwift/EntryController.swift
@@ -0,0 +1,13 @@
+//
+//  EntryController.swift
+//  DayXTrioSwift
+//
+//  Created by Ethan Hess on 5/12/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+
---
+++ DayXTrioSwift/Entry.swift
@@ -0,0 +1,18 @@
+//
+//  Entry.swift
+//  DayXTrioSwift
+//
+//  Created by Ethan Hess on 5/12/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Entry: NSManagedObject {
+
+    @NSManaged var title: String
+    @NSManaged var text: String
+    @NSManaged var date: NSDate
+
+}
