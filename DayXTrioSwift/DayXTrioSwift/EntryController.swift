3ef14cc2ffcf3bf2d17b5638cfea77b7891d3ce6
EthanHess <ech1988@gmail.com>
2015-05-12 16:06:26 -0600

    entry controller with methods

--- DayXTrioSwift/DayXTrioSwift/EntryController.swift
+++ DayXTrioSwift/DayXTrioSwift/EntryController.swift
@@ -10,4 +10,37 @@ import Foundation
 import UIKit
 import CoreData
 
-
+class EntryController : NSObject {
+    
+    var entries:[Entry] {
+        get {
+            return Stack.sharedInstance.managedObjectContext.executeFetchRequest(NSFetchRequest(entityName:"Entry"), error: nil) as! Array
+        }
+    }
+    
+    //shared instance 
+    
+    static let sharedInstance = EntryController ()
+    
+    func createEntryWithDetails(title:String, text:String, date:NSDate) {
+        var entry = NSEntityDescription.insertNewObjectForEntityForName("Entry", inManagedObjectContext: Stack.sharedInstance.managedObjectContext) as! Entry
+        
+        entry.title = title
+        entry.text = text
+        entry.date = date
+        
+        self.save()
+    }
+    
+    func removeEntry(entry:Entry) {
+        
+        entry.managedObjectContext!.deleteObject(entry)
+        self.save()
+    }
+    
+    func save() {
+        
+        Stack.sharedInstance.managedObjectContext.save(nil)
+    }
+    
+}
